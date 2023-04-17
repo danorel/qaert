9040dfded3ede140e348b2ad544126bab756e472
Dan <mail.ordan@gmail.com>
2023-04-17 21:14:42 +0300

    Reformat project files

--- bot.py
+++ bot.py
@@ -6,18 +6,14 @@ from aiogram.utils.executor import start_webhook
 
 from constants import *
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
-# Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
 
 
 @dp.message_handler()
@@ -25,11 +21,9 @@ async def echo(message: types.Message):
     await message.answer(message.text)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=["start", "help"])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm Quaert!\nI am ready to answer your questions!")
-
-# Define hook handlers.
 
 
 async def on_startup(dispatcher):
@@ -55,5 +49,5 @@ def run():
         executor.start_polling(dp, skip_updates=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
