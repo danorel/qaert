59f691d5fdc91bb6acae2a587f4fac3ebf15ba3a
Dan <mail.ordan@gmail.com>
2023-04-18 00:29:12 +0300

    Remove webhook logic: using polling instead

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import logging
 
 from aiogram import Bot, executor, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
@@ -39,27 +38,8 @@ async def answer(message: types.Message):
     await message.answer(answer)
 
 
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
-
-
 def run():
-    if IS_PRODUCTION:
-        start_webhook(
-            dispatcher=dp,
-            webhook_path=WEBHOOK_PATH,
-            skip_updates=True,
-            on_startup=on_startup,
-            on_shutdown=on_shutdown,
-            host=WEBAPP_HOST,
-            port=WEBAPP_PORT,
-        )
-    else:
-        executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == "__main__":
